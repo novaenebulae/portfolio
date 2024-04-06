a09a07c2f72a39455d82c7894687a65ce2c9eba5
novaenebulae <mail.lucas.olivarez@gmail.com>
2024-04-06 02:41:34 +0200

    Mobile version

--- js/tilt.js
+++ js/tilt.js
@@ -7,4 +7,21 @@ export function tilt() {
         gyroscope: false
     }
     );
+
+    let destroyAll = document.querySelectorAll(".js-tilt");
+
+    let firstTime = 0;
+
+    document.addEventListener("touchstart", disableOnMobile);
+
+    function disableOnMobile() {
+        if (firstTime < 1) {
+            destroyAll.forEach(box => {
+                box.vanillaTilt.destroy();
+                console.log("Chargé");
+            });
+            firstTime = firstTime + 1;
+            console.log(firstTime)
+        }
+    }
 }
